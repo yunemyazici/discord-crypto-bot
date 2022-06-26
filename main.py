e1909184fe4f818159640666a56579d70b07faea
Yunus Emre Yazici <104262257+yunemyazici@users.noreply.github.com>
2022-06-26 16:09:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_coin(name):
   }
   headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': '9583d52c-6569-4aaf-8870-9d18b5c0b80e',
+    'X-CMC_PRO_API_KEY': 'YOUR KEY FROM DISCORD',
   }
   
   session = Session()
@@ -48,28 +48,8 @@ async def on_message(message):
   if message.content.startswith("!p"):
     await message.channel.send(f"{name}'s price is ${price}")
   if message.content.startswith("!m"):
-    await message.channel.send(f"{name}'s market cap is ${mcap}")  #market capteki virgülleri kaldır
+    await message.channel.send(f"{name}'s market cap is ${mcap}")
   if "invest" in message.content:
-    await message.channel.send("Be careful when reading someone else's ideas. Make sure you are making your own investment decisions")  
-  secret_coin = "dogecoin" #this will be selected randomly
-  if secret_coin in message.content:
-    await message.channel.send(f"{client.user} found today's coin. it is {secret_coin}")  #
+    await message.channel.send("Be careful when reading someone else's ideas. Make sure you are making your own investment decisions")
 
 client.run("OTg5MTQ4OTc4NjAxOTQ3MTc2.G92gO6.-jRTLSBNfZGvgtVoFlLZkw-ZfBoQD1r7wq7BWw")
-
-
-#bazı durumlarda ünlü birinin sözlerini söyle
-#bazı davranışlarda bulunursa o kişiyi at
-#eğer belli miktarda belli sözü söylediyse veya bir aksiyon aldıysa(beğenme vs) o kişiyi başka bir kanala ekle
-
-
-
-#crypto fiyatını discorda kullanıcı olarak gibi sabitleme
-#rastgele bir coin hakkında bilgi alma fiyat market cap coin açıklaması vs
-
-
-#scoreboard yatırım tarzı bir şey olabilir mi değerlendir
-
-
-#bazı twitter hesaplarından haberleri direk haber kanalına paylaş
-#i will have to use tweepy module read the docs 
